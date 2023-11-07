2138616d218aeefdfabf6a5e134df1c99469448a
stv10 <santiagovillagratecnica1@gmail.com>
2023-11-06 21:31:40 -0300

    full crud for invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -39,3 +39,32 @@ export async function createInvoice(formData: FormData) {
 
   //   console.log(rawFormData);
 }
+
+// Use Zod to update the expected types
+const UpdateInvoice = InvoiceSchema.omit({ date: true });
+
+// ...
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE from invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+}
